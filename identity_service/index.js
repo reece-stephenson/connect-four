243a20f136c568ef488efa1178ffc6759a443c7f
reece-stephenson <reeces@bbd.co.za>
2023-06-22 21:58:11 +0200

    Add rate limiter recommendation

--- identity_service/index.js
+++ identity_service/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import createCredentialsHandler from "./handlers/create-credentials.js";
 import exchangeCredentialsHandler from "./handlers/exchange-credentials.js";
 import bearer from "./middleware/verify-bearer.js";
+import RateLimit from "express-rate-limit";
 
 config();
 connect();
@@ -12,6 +13,16 @@ const app = express();
 const port = process.env.PORT || 4001;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// set up rate limiter
+var limiter = RateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 20
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
 
 app.post("/create-credentials", createCredentialsHandler);
 app.post("/exchange-credentials", exchangeCredentialsHandler);
