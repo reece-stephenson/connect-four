7928e6c7853a83424d0bf330d6c942bc885e1335
reece-stephenson <reeces@bbd.co.za>
2023-06-24 23:32:37 +0200

    Add authentication to web socket connection

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,7 +1,9 @@
 import { WebSocketServer } from "ws";
 import { createGame, joinGame } from "./game.js";
-import { setupGame,clientMove,updateGameState } from "./serverGameLogic.js";
+import { setupGame, clientMove, updateGameState } from "./serverGameLogic.js";
+import JSONWebToken from "jsonwebtoken";
 import http from "http";
+import Token from "../model/token.js";
 import { config } from "dotenv";
 import app from "../app.js";
 
@@ -16,7 +18,61 @@ server.listen(PORT, () => {
   );
 });
 
-const wss = new WebSocketServer({ server: server });
+const wss = new WebSocketServer({
+  verifyClient: async function (info, cb) {
+    var token = info.req.headers.cookie;
+    token = token.replace('token=', '')
+
+    if (!token)
+      cb(false, 401, 'Unauthorized')
+    else {
+      // Decode our token so we can 
+      const decoded = JSONWebToken.decode(token, { complete: true });
+
+      if (!decoded || !decoded.header.kid) {
+        // No kid, no pass, we didn't generate this
+        cb(false, 401, 'Unauthorized')
+        return;
+      }
+
+      const verifiedToken = await Token.findOne({ jwtKey: decoded.header.kid });
+
+      if (!verifiedToken) {
+        // No token, no pass
+        cb(false, 401, 'Unauthorized')
+        return;
+      }
+
+      if (!(verifiedToken && verifiedToken.algorithm && verifiedToken.publicKey)) {
+        // No key information to compare to
+        cb(false, 401, 'Unauthorized')
+        return;
+      }
+
+      const verified = await new Promise(
+        resolve => JSONWebToken.verify(
+          token,
+          verifiedToken.publicKey,
+          {
+            algorithms: [
+              // Only allow the one that was stored with the key 
+              verifiedToken.algorithm
+            ]
+          },
+          (error, verified) => resolve(error ? undefined : verified)
+        )
+      );
+      if (!verified) {
+        // Not valid 
+        cb(false, 401, 'Unauthorized')
+        return;
+      }
+
+      cb(true);
+    }
+  },
+  server: server
+});
 
 function parseMessage(msg, ws) {
   switch (msg['requestType']) {
@@ -42,5 +98,6 @@ function parseMessage(msg, ws) {
 }
 
 wss.on("connection", (ws) => {
+  console.log("Client connected!");
   ws.on("message", (msg) => parseMessage(JSON.parse(msg), ws));
 });
