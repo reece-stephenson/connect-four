1551cc3cb65fb7f4bfa1ea872f02afba8853b8aa
Karishma George <karishmag@bbd.co.za>
2023-06-27 16:44:38 +0200

    fixed request vulnerability

--- frontend/routes/public.route.js
+++ frontend/routes/public.route.js
@@ -1,69 +1,62 @@
 import { Router } from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
-import request from "request";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const publicRouter = Router();
 
-publicRouter.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../views/welcomeScreen.html'));
+publicRouter.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../views/welcomeScreen.html"));
 });
 
-publicRouter.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../views/registerScreen.html'));
+publicRouter.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../views/registerScreen.html"));
 });
 
-publicRouter.post('/signup', (req, res) => {
-    request.post(
-        {
-            url: process.env.ID_URL + '/create-credentials',
-            json: req.body,
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': '*/*'
-            },
-            credentials: 'include'
-        },
-        function (error, response, body) {
-            if (response.statusCode == 200) {
-                res.status(response.statusCode).send(body);
-            }
-            else {
-                res.status(response.statusCode).send(body);
-            }
-        });
+publicRouter.post("/signup", (req, res) => {
+  fetch(process.env.ID_URL + "/create-credentials", {
+    Method: "POST",
+    body: req.body,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "*/*",
+    },
+    credentials: "include",
+  }).then((error, response, body) => {
+    if (response.statusCode == 200) {
+      res.status(response.statusCode).send(body);
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
 });
 
-publicRouter.post('/login', (req, res) => {
-    request.post(
-        {
-            url: process.env.ID_URL + '/exchange-credentials',
-            json: req.body,
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': '*/*'
-            },
-            credentials: 'include'
-        },
-        function (error, response, body) {
-            if (response.statusCode == 200) {
-                let options = {
-                    httpOnly: true,
-                    secure: true,
-                    sameSite: 'strict'
-                }
-                res.cookie('token', body.token, options);
-                res.cookie('email', body.email, options);
-                res.cookie('username', body.username, options);
+publicRouter.post("/login", (req, res) => {
+  fetch(process.env.ID_URL + "/create-credentials", {
+    Method: "POST",
+    body: req.body,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "*/*",
+    },
+    credentials: "include",
+  }).then((error, response, body) => {
+    if (response.statusCode == 200) {
+      let options = {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+      };
+      res.cookie("token", body.token, options);
+      res.cookie("email", body.email, options);
+      res.cookie("username", body.username, options);
 
-                res.sendStatus(200);
-            }
-            else {
-                res.status(response.statusCode).send(body);
-            }
-        });
+      res.sendStatus(200);
+    } else {
+      res.status(response.statusCode).send(body);
+    }
+  });
 });
 export default publicRouter;
